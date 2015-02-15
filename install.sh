560da42858228515bff1de0e2cc3219d3cd93896
momentspace <pi@raspberrypi.(none)>
2015-02-15 14:03:37 +0000

    modify install.sh to additional comment

--- install.sh
+++ install.sh
@@ -2,4 +2,13 @@
 mkdir /mnt/usb_memory
 cp shell/storage_configure.sh /usr/local/bin/
 cp rules/50-usb-memory.rules /etc/udev/rules.d/
-
+echo ""
+echo "target wlan device(ex. wlan0) setting off to interfaces."
+echo " and killall wpa_supplicant."
+echo ""
+echo " -- interfaces --"
+echo " # iface wlan0 inet manual "
+echo " # wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf"
+echo " -- killall wpa_supplicant --"
+echo " # sudo killall wpa_supplicant"
+echo ""
